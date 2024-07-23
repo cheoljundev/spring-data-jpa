f71343b91d794370b44539f1efe3bdbdbf114b8d
cheoljundev <cheoljundev@gmail.com>
2024-07-24 03:29:42 +0900

    JPA Hint & Lock

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -261,4 +261,29 @@ class MemberRepositoryTest {
 
         //then
     }
+    
+    @Test
+    public void queryHint() {
+        // given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2"); // readOnly 라서 변경 감지가 되지 않는다.
+
+        em.flush();
+        //then
+    }
+    @Test
+    public void lock() {
+        // given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+        //when
+        List<Member> result = memberRepository.findLockByUsername("member1");
+
+        //then
+    }
 }
